a1df0ef47d992ed7cab95feef2b63c9666afbd9d
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-11-12 09:08:34 +0900

    /models/* の実装

---
+++ app/models/food.rb
@@ -0,0 +1,5 @@
+class Food <  ApplicationRecord
+  belongs_to :restaurant
+  belongs_to :orders, optional: true
+  has_one :line_food
+end
---
+++ app/models/order.rb
@@ -0,0 +1,16 @@
+class Order <ApplicationRecord
+  has_many :line_foods
+
+  validates :total_price, numericality: { greater_than: 0 }
+
+  def save_with_update_line_foods!(line_foods)
+    # MEMO: block内すべてが成功した場合に変更がDBに保存される | エラー発生時は変更をロールバックする
+    # MEMO: 例外発生時、ActiveRecord::Rollback 例外エラーを返す
+    ActiveRecord::Base.transaction do
+      line_foods.each do |line_food|
+        line_food.update(active: false, order: self) # MEMO: order属性を現在の Orderインスタンス（self）に設定
+      end
+      self.save!
+    end
+  end
+end
