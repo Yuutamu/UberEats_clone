3b84c7161fa4c50441903abc52e4cefd98574937
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-11-15 08:29:07 +0900

    DBにテストデータを注入

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resorces :restaurants do
-        resorces :food, only: %i[index]
+      resources :restaurants do
+        resources :food, only: %i[index]
       end
-      resorces :line_food, only: %i[index create]
+      resources :line_food, only: %i[index create]
       put 'line_foods/replace', to: 'line_foods#replace' # garape-gem の put-end ブロック putで叩かれたときに呼ばれる
-      resorces :orders, only: %i[create]
+      resources :orders, only: %i[create]
     end
   end
 end
