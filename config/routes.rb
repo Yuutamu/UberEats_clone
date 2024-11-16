0179469a01e31746956718d2da497ac2619250d7
Yuutamu <iwamoto.yuta1925@gmail.com>
2024-11-16 18:56:54 +0900

    foods_controller実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :restaurants do
-        resources :food, only: %i[index]
+        resources :foods, only: %i[index]
       end
       resources :line_food, only: %i[index create]
       put 'line_foods/replace', to: 'line_foods#replace' # garape-gem の put-end ブロック putで叩かれたときに呼ばれる
